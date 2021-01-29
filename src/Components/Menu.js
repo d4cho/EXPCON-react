983256ba5ab2a55d61accdc9f02d8cf501c0c96d
Daniel Cho <danielcho91@gmail.com>
2021-01-28 22:06:39 -0500

    menu button

---
+++ src/Components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Menu.css';
+
+const Menu = () => {
+  return (
+    <div className='Menu' style={{ color: '#fff' }}>
+      <i class='fas fa-bars'></i> EXP|CON
+    </div>
+  );
+};
+
+export default Menu;
--- src/Pages/PricingPage/PricingPage.js
+++ src/Pages/PricingPage/PricingPage.js
@@ -3,10 +3,12 @@ import React from 'react';
 import Footer from '../../Components/Footer';
 import Perks from './sections/Perks';
 import Pricing from './sections/Pricing';
+import Menu from '../../Components/Menu';
 
 const PricingPage = () => {
   return (
     <div>
+      <Menu />
       <Pricing />
       <Perks />
       <Footer />
