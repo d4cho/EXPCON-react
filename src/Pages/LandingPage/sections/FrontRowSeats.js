b8ab3ae18c393bbde0011274c936fdee2df9ffdf
Daniel Cho <danielcho91@gmail.com>
2021-01-30 17:44:46 -0500

    front row section

--- src/Pages/LandingPage/sections/FrontRowSeats.js
+++ src/Pages/LandingPage/sections/FrontRowSeats.js
@@ -8,19 +8,16 @@ const FrontRowSeats = () => {
   const test = useRef(null);
 
   const handleMouseMove = (e) => {
-    // debugger;
     let magic = test;
     let magicWHalf = magic.current.offsetWidth / 2;
 
     magic.current.style.left = e.pageX - magicWHalf + 'px';
-    magic.current.style.top =
-      parseInt(e.pageY - e.target.offsetTop - magicWHalf) + 'px';
-    console.log(parseInt(e.pageY - e.target.offsetTop - magicWHalf));
+    magic.current.style.top = e.screenY - magicWHalf * 3.8 + 'px';
   };
 
   return (
     <div className={styles.container} onMouseMove={handleMouseMove}>
-      {/* <div className={styles.button}>
+      <div className={styles.button}>
         <HoverButton color='#FFB33F' text='TRY IT NOW' link='/pricing' />
       </div>
       <div className={styles.contentContainer}>
@@ -28,10 +25,10 @@ const FrontRowSeats = () => {
         <h1 className={styles.subheading}>
           Experience concerts up close and personal.
         </h1>
+        <div className={styles.buttontwo}>
+          <HoverButton color='#FFB33F' text='SEE DEMO' />
+        </div>
       </div>
-      <div>
-        <HoverButton color='#FFB33F' text='SEE DEMO' />
-      </div> */}
       <div
         ref={test}
         className={styles.magic}
