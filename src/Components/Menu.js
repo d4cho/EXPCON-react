cd303464bdd033945e1510a35d42b58450a6a85a
Daniel Cho <danielcho91@gmail.com>
2021-02-01 15:30:01 -0500

    menu button

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './Menu.module.css';
 
 const Menu = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const color = props.color ? props.color : '#fff';
 
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className={styles.Menu} style={{ color }}>
-      <i className='fas fa-bars'></i> EXP|CON
+    <div>
+      <div className={isOpen && styles.Backdrop} onClick={handleClick} />
+      <div className={isOpen ? styles.CircleBig : styles.Circle} />
+      <div className={styles.Menu} style={{ color }}>
+        <i
+          className='fas fa-bars'
+          onClick={handleClick}
+          style={{ cursor: 'pointer' }}
+        />
+        <div style={{ paddingLeft: '1rem' }}>EXP|CON</div>
+      </div>
     </div>
   );
 };
