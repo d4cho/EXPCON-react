79275173e6b323bddff7edaef6b6796c6fcf034f
Daniel Cho <danielcho91@gmail.com>
2021-02-02 14:43:17 -0500

    review section layout

--- src/Pages/LandingPage/sections/Reviews.js
+++ src/Pages/LandingPage/sections/Reviews.js
@@ -12,9 +12,10 @@ const Reviews = () => {
         <HoverButton color='#1FE1E9' text='TRY IT NOW' link='/pricing' />
       </div>
       <div className={styles.contentContainer}>
-        <img className={styles.img1} src={speaker1} alt='speaker1' />
-        <img className={styles.img2} src={speaker2} alt='speaker2' />
-
+        <div className={styles.imageContainer}>
+          <img className={styles.img1} src={speaker1} alt='speaker1' />
+          <img className={styles.img2} src={speaker2} alt='speaker2' />
+        </div>
         <div className={styles.rightContentContainer}>
           <h1 className={styles.heading}>REVIEWS</h1>
           <div className={styles.reviewsContainer}>
