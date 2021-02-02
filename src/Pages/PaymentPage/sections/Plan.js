946f7ee88bcc2191a69816bf2d2d3950afb93a53
Daniel Cho <danielcho91@gmail.com>
2021-02-02 16:07:46 -0500

    custom radio control buttons

--- src/Pages/PaymentPage/sections/Plan.js
+++ src/Pages/PaymentPage/sections/Plan.js
@@ -11,32 +11,43 @@ const Plan = (props) => {
     <div>
       <div className={styles.subheading}>1. Select your plan</div>
       <form className={styles.radioContainer} onChange={handleChange}>
-        <label className={styles.radioBasic}>
-          <input
-            type='radio'
-            name='plan'
-            value='basic'
-            defaultChecked={props.plan === 'BASIC' ? true : false}
-          />
-          BASIC
+        <label className={styles.radio} style={{ color: '#d24848' }}>
+          <span className={styles.radioInput}>
+            <input
+              type='radio'
+              name='plan'
+              value='basic'
+              defaultChecked={props.plan === 'BASIC' ? true : false}
+            />
+            <span className={styles.radioControl}></span>
+          </span>
+          <span className={styles.radioLabel}>BASIC</span>
         </label>
-        <label className={styles.radioAdvanced}>
-          <input
-            type='radio'
-            name='plan'
-            value='advanced'
-            defaultChecked={props.plan === 'ADVANCED' ? true : false}
-          />
-          ADVANCED
+
+        <label className={styles.radio} style={{ color: '#ffb33f' }}>
+          <span className={styles.radioInput}>
+            <input
+              type='radio'
+              name='plan'
+              value='advanced'
+              defaultChecked={props.plan === 'ADVANCED' ? true : false}
+            />
+            <span className={styles.radioControl}></span>
+          </span>
+          <span className={styles.radioLabel}>ADVANCED</span>
         </label>
-        <label className={styles.radioPro}>
-          <input
-            type='radio'
-            name='plan'
-            value='pro'
-            defaultChecked={props.plan === 'PRO' ? true : false}
-          />
-          PRO
+
+        <label className={styles.radio} style={{ color: '#1fe1e9' }}>
+          <span className={styles.radioInput}>
+            <input
+              type='radio'
+              name='plan'
+              value='pro'
+              defaultChecked={props.plan === 'PRO' ? true : false}
+            />
+            <span className={styles.radioControl}></span>
+          </span>
+          <span className={styles.radioLabel}>PRO</span>
         </label>
       </form>
     </div>
